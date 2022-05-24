5fc652ca9c28f3f9874a1b15e4616b5049cd3ca7
Manish Upadhyay <upadhyaymanish302@gmail.com>
2022-05-25 00:15:36 +0530

    created message routes to access messages by chats

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,13 @@ const express = require("express");
 const connectDB = require("./Config/db"); 
 const userRoutes= require("./Routes/userRouters");
 const chatRoutes = require("./Routes/chatRoutes")
+const messageRoutes = require("./Routes/messageRoutes")
 
 connectDB();
 app.use(express.json());
 
 app.use('/api/user',userRoutes)
 app.use('/api/chat',chatRoutes);
+app.use('/api/message',messageRoutes);
 const PORT=process.env.PORT
 app.listen(PORT,console.log(`server started at port ${PORT}`))
